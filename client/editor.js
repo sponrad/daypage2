bf43c712c305f94d38200e5dfe3888d43ba391b6
Conrad <conrad@devlabtech.com>
2013-12-02 15:12:34 -0800

    entry list updating based on datepicker, widened writing area

--- client/editor.js
+++ client/editor.js
@@ -2,55 +2,117 @@ Entries = new Meteor.Collection('entries')
 Days = new Meteor.Collection('days') //Why have this if there is a datestamp on entries
 
 if (Meteor.isClient) {    
-
     Template.sideBar.date = function(){
 	var d = new Date().toLocaleDateString();
 	return d;
     }
+	
+	Template.sideBar.entries = function(){
+		date = Session.get("selectedDate");
+		if (!date)
+			return {};
+		return Entries.find({date: date});
+	}
+	
+	Template.sideBar.events({
+		'blur #thisday': function(event){
+			//console.log("thisday blurred");
+		},
+		'click .entryListItem': function(event){
+			console.log("clicked");
+			console.log( this._id );
+			entry = Entries.findOne( this._id);
+			console.log(entry);
+			$("writingbox").val( entry.content );
+		}
+		
+	});
 
     Template.editor.rendered = function(){
-	var editor = new wysihtml5.Editor("writingbox", {
-	    toolbar:      "toolbar",
-	    stylesheets:  "wysihtml5-stylesheet.css",
-	    parserRules:  wysihtml5ParserRules
-	});	
-
-/*	editor.on('load', function() {
-	    // The wysiwyg editor is in the DOM. It's safe to make the plugin call
-	    $(editor.composer.iframe).wysihtml5_size_matters();
-	}); */
-
+		var editor = new wysihtml5.Editor("writingbox", {
+			toolbar:      "toolbar",
+			stylesheets:  "wysihtml5-stylesheet.css",
+			parserRules:  wysihtml5ParserRules
+		});
+		$("#thisday").datepicker({
+		onSelect: function(selectedDate){
+		    thisday = new Date(selectedDate)
+		    tdatestring =  thisday.getMonth()+1 + "/" + thisday.getDate() + "/" + thisday.getFullYear();
+		    $('#thisday').val(tdatestring);
+			Session.set("selectedDate", $("#thisday").val() );
+			}
+		});
     }
 
+    Template.editor.events({
+	'keydown': function(e){
+	    if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)){
+		e.preventDefault();
+		console.log("save event");
+		console.log( $('#writingbox').val() );
+		date = $("#thisday").val();
+		entry = setEntry(date);
+		Meteor.setTimeout( function(){
+/*		    Entries.update(
+			entry._id, 
+			{$set: {content: $('#writingbox').val()}}
+		    ); */
+		    console.log(entry);
+		    $("#entry-saved").show();
+		    $("#entry-save").hide();
+		}, 100);
+	    }
+	},
+	
+	'blur #writingbox': function(e){
+	    console.log("writingbox changed");
+	    $("#entry-saved").hide();
+	    $("#entry-save").show();
+	},
+	
+	'click #entry-save': function(e){
+	    console.log("save event");
+	    date = $("#thisday").val();
+	    entry = setEntry(date);
+	    Meteor.setTimeout( function(){
+		Entries.update(
+		    entry._id, 
+		    {$set: {content: $('#writingbox').val()}}
+		);
+		console.log(entry);
+		//$("#entry-saved").show();
+		//$("#entry-save").hide();
+		$('#writingbox').focus();
+	    }, 100);
+	    
+	}
+    });
+    
 ////////////////////////////////stuff below may not stay
 
     function setEntry(date){
 	entry = Entries.findOne({user: Meteor.userId(), date: date});
 	//console.log(entry);
 	if (entry){
-	    localStorage.setItem("entry-id", entry._id);
-	    localStorage.setItem("selected-date", date);
-	    Session.set("entry-id", entry._id);
-	    Session.set("selected-date", date);
+	    localStorage.setItem("entryId", entry._id);
+	    localStorage.setItem("selectedDate", date);
+	    Session.set("entryId", entry._id);
+	    Session.set("selectedDate", date);
 	}
 	else {
 	    entry = Entries.insert({
 		user: Meteor.userId(), 
 		date: date, 
 	    });
-	    localStorage.setItem("entry-id", entry._id);
-	    localStorage.setItem("selected-date", date);
-	    Session.set("entry-id", entry._id);
-	    Session.set("selected-date", date);
+	    localStorage.setItem("entryId", entry._id);
+	    localStorage.setItem("selectedDate", date);
+	    Session.set("entryId", entry._id);
+	    Session.set("selectedDate", date);
 	}
 	return entry
     }
 
     Template.userwrite.rendered = function(){
-	var editor = new wysihtml5.Editor("writingbox", {
-	    toolbar: "wysihtml5-toolbar",
-	    parserRules: wysihtml5ParserRules
-	    });
 	thisday = new Date()
 	datestring =  thisday.getMonth()+1 + "/" + thisday.getDate() + "/" + thisday.getFullYear();
 	entry = setEntry(datestring);
@@ -166,8 +228,8 @@ if (Meteor.isClient) {
     });
 
     Template.entryselect.entries = function(){
-	date = localStorage.getItem("selected-date");
-//	date = Session.get("selected-date");
+	date = localStorage.getItem("selectedDate");
+//	date = Session.get("selectedDate");
 	entries = new Array();
 	Entries.find({user: Meteor.userId(), date: date}).forEach(function(entry){
 	    entries.push({
